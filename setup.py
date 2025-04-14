5ee50fd0ee206c31191a91fe670767e0080cc608
sorea1k <sorea1k@163.com>
2025-04-14 14:31:18 +0800

    about a tiny problem

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 from setuptools import setup, Extension
-import os
 import pybind11
-import platform
+from setuptools.command.build_ext import build_ext
+
+class BuildExt(build_ext):
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        if ct == 'msvc':
+            for ext in self.extensions:
+                ext.extra_compile_args = ["/std:c++17", "/arch:AVX2", "/fp:fast"]
+        else:
+            for ext in self.extensions:
+                ext.extra_compile_args = ["-std=c++17", "-mavx2", "-ffast-math"]
+        build_ext.build_extensions(self)
 
 module_name = "dng_graph"
 source_files = ["src/dng.cpp"]
 include_dirs = ["src", pybind11.get_include()]
 
-# 自动判断平台来设置编译参数
-if platform.system() == "Windows":
-    compile_args = ["/std:c++17", "/arch:AVX2", "/fp:fast"]
-else:
-    compile_args = ["-std=c++17", "-mavx2", "-ffast-math"]
-
 dng_graph_module = Extension(
     module_name,
     sources=source_files,
     include_dirs=include_dirs,
-    extra_compile_args=compile_args,
     language="c++"
 )
 
@@ -26,6 +29,7 @@ setup(
     version="1.0.0",
     description="A C++ library for graph-based nearest neighbor search",
     ext_modules=[dng_graph_module],
+    cmdclass={"build_ext": BuildExt},
     classifiers=[
         "Programming Language :: C++",
         "License :: OSI Approved :: MIT License",
